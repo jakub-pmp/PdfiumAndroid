d8d5b1c890e91a58037e47ab861d64a4f00d9bd8
Jakub Mirota <mirota.jakub@gmail.com>
2024-10-24 17:52:45 +0200

    Updates project structure

---
+++ pdfium-android/build/generated/source/buildConfig/release/com/shockwave/pdfium/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.shockwave.pdfium;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.shockwave.pdfium";
+  public static final String BUILD_TYPE = "release";
+}
